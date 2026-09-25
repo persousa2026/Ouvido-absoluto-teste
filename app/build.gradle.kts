dc0a62adac1828a6bc5f3f2a996a1f7adb0e4c33
persousa2026 <325393678+persousa2026@users.noreply.github.com>
2026-09-25 15:01:20 -0300

    Atualizar versão do aplicativo para 0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ouvidoabsoluto.ai"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     buildFeatures {
